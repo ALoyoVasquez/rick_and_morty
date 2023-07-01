e2bf2cb8c3cd6c7c502301b4c9928a4801fdaf15
ALoyoVasquez <anitamlv@gmail.com>
2023-07-01 10:32:51 -0400

    test

--- Server/test/index.test.js
+++ Server/test/index.test.js
@@ -32,6 +32,7 @@ describe("Test de RUTAS", () => {
       );
       expect(response.body.access).toBeTruthy(); //.toEqual(access);
     });
+
     it("Access False - Credenciales No Válidas.", async () => {
       const response = await agent.get(
         "/rickandmorty/login?email=ani@mail.com&password=123456"
@@ -43,31 +44,44 @@ describe("Test de RUTAS", () => {
   //? Ejercicio 6
   describe("POST /rickandmorty/fav", () => {
     it("Debe guardar el personaje en favoritos", async () => {
-      const response = await agent.post("/rickandmorty/fav")
+      const response = (await agent.post("/rickandmorty/fav")
       .send({
-        id: 114,
-        name: "Ethan",
-        status: "unknown",
-        species: "Human",
-        gender: "Male",
-        origin: { name: "Earth (C-137)" },
-        image: "https://rickandmortyapi.com/api/character/avatar/114.jpeg",
-      });
-      expect(response.body).toContainEqual({
-        id: 114,
-        name: "Ethan",
-        status: "unknown",
-        species: "Human",
-        gender: "Male",
-        origin: { name: "Earth (C-137)" },
-        image: "https://rickandmortyapi.com/api/character/avatar/114.jpeg",
+        id: 1,
+        name: "Ana",
+        // status: "unknown",
+        // species: "Human",
+        // gender: "Male",
+        // origin: { name: "Earth (C-137)" },
+        // image: "https://rickandmortyapi.com/api/character/avatar/114.jpeg",
+      })).body;
+      expect(response).toContainEqual({
+        id: 1,
+        name: "Ana",
+        // status: "unknown",
+        // species: "Human",
+        // gender: "Male",
+        // origin: { name: "Earth (C-137)" },
+        // image: "https://rickandmortyapi.com/api/character/avatar/114.jpeg",
       });
     });
   });
 
   describe("DELETE /rickandmorty/fav/:id", () => {
-    it("Debe agregar personajes en favoritos sin eliminar los existentes", async () => {
-       
+    const character1 = {id: 1, name: 'Ana'}
+    const character2 = {id: 2, name: 'Samuel'}
+
+    it("Devuelve  personajes en favoritos sin eliminar los existentes", async () => {
+       const response = await agent.get(
+        "/rickandmorty/fav/56"
+      );
+      expect(response.body).toContainEqual(character1)
      })
+
+     it("Devuelve todo el arreglo sin el Personaje eliminado", async () => {
+      const response = await agent.get(
+       "/rickandmorty/fav/1"
+     );
+     expect(response.body).not.toContainEqual(character1); 
+    })
   });
 });
