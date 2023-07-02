8a8c737735a8156f309d3efb57f91aab7d8dae9c
ALoyoVasquez <anitamlv@gmail.com>
2023-07-02 17:41:48 -0400

    intento de responsive

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -1,25 +1,22 @@
-import SearchBar from "./SearchBar/SearchBar.jsx"
-import style from "./Nav.module.css"
-import { Link  } from "react-router-dom";
+import SearchBar from "./SearchBar/SearchBar.jsx";
+import style from "./Nav.module.css";
+import { Link } from "react-router-dom";
 // About
-import image from '../../imgs/logo-name.png'
+import image from "../../imgs/logo-name.png";
 
 export default function Nav(props) {
+  const { onSearch, logOut } = props;
+  // const {logOut}
 
-const {onSearch, logOut} = props;
-// const {logOut}
-
-return (
-        <nav className={style.container}>
-            
-            <img src={image} alt="" className={style.img} />
-
-            <Link to="/home" className={style.link}>Home</Link>
-            <Link to="/views/About/about" className={style.link}>About</Link>
-            {/* <h3> Bienvenido , </h3> */}
-            <Link to="/favorites" className={style.link}>Favorites</Link>
-            <Link to="/" className={style.link} params={ logOut }>Log_Out</Link>
-            <SearchBar onSearch={onSearch} />
-        </nav>
-   );
+  return (
+    <nav className={style.container}>
+      <img src={image} alt="" className={style.img} />
+      <Link to="/home" className={style.link}> Home </Link>
+      <Link to="/views/About/about" className={style.link}> About</Link>
+      {/* <h3> Bienvenido , </h3> */}
+      <Link to="/favorites" className={style.link}> Favorites </Link>
+      <Link to="/" className={style.link} params={logOut}> Log_Out </Link>
+      <SearchBar onSearch={onSearch} />
+    </nav>
+  );
 }
